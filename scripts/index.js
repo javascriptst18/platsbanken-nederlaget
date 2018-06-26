af285ced72da9447abdf2a850c48b2e5613c5731
Malcolm Horal <malcolm.horal@gmail.com>
2018-06-26 17:03:18 +0200

    added nonfunctional search functions again!

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,18 @@ async function runScript() {
   //variables ("glocals")
   const cardContainer = document.querySelector(`#cardContainer`);
 
-  //presumtions
+  //initiate search results.
   let criteria = concatCriterias();
   const results = await searchByCriteria(`platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=10`);
-  console.log(results.matchningslista.matchningdata[0].annonsrubrik);
+  console.log(results)
   printJobList(results.matchningslista.matchningdata)
 
+  // to be announced: 
+  // addNumberOfJobs(results.matchningslista.antal_platsannonser, results.matchningslista.antal_platserTotal, results.matchningslista.antal_sidor);
+  console.log(results.matchningslista.antal_platsannonser);
+  console.log(results.matchningslista.antal_sidor);
+
+  createListners();
 }
 // runs script
 runScript();
@@ -34,24 +40,56 @@ function concatCriterias(lanid = `1`, yrkesomradeid = `3`, antalrader = `10`) {
 async function searchByCriteria(searchCriteria) {
   //sets base URL:
   const baseURL = `http://api.arbetsformedlingen.se/af/v0/`;
-  //fethces adn awaits response object
+  //fetches and awaits response object
   const responseObject = await fetch(baseURL + searchCriteria);
   const matches = await responseObject.json();
-  console.log(matches);
+
   return (matches);
 }
 
 function printJobList(jobList) {
+  // erase list
   cardContainer.innerHTML = "";
-
+  
+  // loop through results.
   for (let jobs of jobList) {
     cardContainer.innerHTML += ` <div id="${jobs.id}" class="card" style="width: 18rem;">
-      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+      <img class="card-img-top" src="images/favicon.png" alt="Card image cap">
       <div class="card-body">
         <h5 class="card-title">${jobs.annonsrubrik}</h5>
-        <p class="card-text">${jobs.kommunnnamn}, ${jobs.lan}</p>
+        <p class="card-text">${jobs.kommunnamn}, ${jobs.lan}</p>
         <a href="${jobs.annonsurl}" class="btn btn-primary">Annonsen hos AF</a>
       </div>
     </div>`
-      }
+  }
 }
+
+function addNumberOfJobs(antalTraffar, antalPerSida, antalSidor) {
+  //adds details to page about search result numbers
+  document.querySelector(`#quantityOfAdsDiv`).innerHTML = antalTraffar + antalPerSida, antalSidor;
+  // adjust row above and add the numbers to the right row!
+}
+
+// designated function that adds listners to buttons and search fields.
+function createListners() {
+  //adds submit lsitener to searhc fild.
+  document.querySelector(`#userInput`).addEventListener(`submit`, submitSearch);
+
+  //add dropdowns
+
+  //add buttons
+
+}
+
+//Create search functionallity
+function submitSearch(event) {
+  event.preventDefault()
+  const searchText = event.target.value;
+  console.log(searchText);
+  if (searchText == ``) {
+    console.log('No search term entered');
+  }
+  //no, this function won't do it! rethink random input!
+  concatCriterias();
+
+};
