cc5984d5c63f31cfe52b208ad3551f8d942c07d7
Malcolm Horal <malcolm.horal@gmail.com>
2018-06-27 14:49:56 +0200

    all dropdwons is working. changed their hard dropdown IDs

--- scripts/index.js
+++ scripts/index.js
@@ -6,33 +6,19 @@ async function runScript() {
   const cardContainer = document.querySelector(`#cardContainer`);
 
   //initiate search results.
-  let criteria = concatCriterias();
-  const results = await searchByCriteria(`platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=10`);
+  const results = await searchByCriteria(`lanid=1&yrkesomradeid=3&antalrader=10`);
   console.log(results);
   printJobList(results.matchningslista.matchningdata)
 
   // to be announced: 
   // addNumberOfJobs(results.matchningslista.antal_platsannonser, results.matchningslista.antal_platserTotal, results.matchningslista.antal_sidor);
 
-  createListners();
+  createListeners();
 }
 // runs script
 runScript();
 
-
 // ALL FUNCTIONS BELOW
-
-
-//concatenates a criteria string to be attatched to baseURL
-// needs to be improved further on!
-function concatCriterias(lanid = `1`, yrkesomradeid = `3`, antalrader = `10`) {
-  const lan = `lanid=${lanid}`;
-  const yrkesOmrade = `yrkesområdeid=${yrkesomradeid}`;
-  const antalRader = `antalrader=${antalrader}`;
-
-  const criterias = `platsannonser/matchning?${lan}&${yrkesOmrade}&${antalRader}`
-  return criterias;
-}
 
 //simple search by concatenated search criteria
 async function searchByCriteria(searchCriteria) {
@@ -47,13 +33,9 @@ function printJobList(jobList) {
   // erase list
   cardContainer.innerHTML = "";
 
-  // loop through results.
+  // loop through results and srite innerHTML.
   for (let jobs of jobList) {
-    //remove style!!!!
-    //
-    // 
-    // 
-    cardContainer.innerHTML += ` <div id="${jobs.annonsid}" class="card" style="width: 60%; height: 12rem;">
+    cardContainer.innerHTML += ` <div id="${jobs.annonsid}" class="card">
       <img class="card-img  " src="http://api.arbetsformedlingen.se/af/v0/platsannonser/${jobs.annonsid}/logotyp" alt="Card image cap">
       <div class="card-body">
         <h5 class="card-title">${jobs.annonsrubrik}</h5>
@@ -74,48 +56,102 @@ function addNumberOfJobs(antalTraffar, antalPerSida, antalSidor) {
 }
 
 // designated function that adds listners to buttons and search fields.
-function createListners() {
+function createListeners() {
   //adds submit lsitener to search fild.
   console.log(`creating listeners...`)
   const searchField = document.getElementById(`formField`);
   searchField.addEventListener(`submit`, submitSearch);
+  const lanMenu = document.getElementById(`dropdownMenuLan`);
+  lanMenu.addEventListener(`click`, submitLan);
+ 
+  const yrkesMenu = document.getElementById(`dropdownMenuYrkesomrade`);
+  yrkesMenu.addEventListener(`click`, submitYrke);
   console.log(`created listeners!`)
-  //add dropdowns
-
-  //add buttons
 
 }
 
 //Create search functionallity
 async function submitSearch(event) {
-  console.log(`event registrerat!`)  
+  //prevents site from reloading!
   event.preventDefault();
+  //get search field elelmtn
   let searchInput = document.getElementById(`userInput`);
+
+  //handles empty case!
   if (searchInput.value == ``) {
-      return searchInput.placeholder = `Prova igen`;
+    // if empty asks for new search!
+    return searchInput.placeholder = `Prova igen`;
   } else {
-    console.log(`gick hem!`) ;
+    //search API for word!
     const searchResults = await searchByWords(searchInput.value);
-    console.log(searchResults) ;
     printJobList(searchResults.matchningslista.matchningdata);
   }
 }
 
+// finds Lan id from Lan data!
+async function submitLan(event) {
+  event.preventDefault();
+  let lanInput = event.target.id;
+  console.log(`Klickat på län: ${lanInput}`);
+  //CHECK LAN ID!!!
+  const lanList = await searchByLan(lanInput);
+  console.log(lanList);
+  printJobList(lanList.matchningslista.matchningdata);
+}
+
+// find job based on yrkesområde!
+async function submitYrke(event) {
+  event.preventDefault();
+  let yrkesInput = event.target.id;
+  console.log(`Klickat på yrkesområde: ${yrkesInput}`);
+  //CHECK LAN ID!!!
+  const yrkesList = await searchByYrkesomrade(yrkesInput);
+  console.log(yrkesList);
+  printJobList(yrkesList.matchningslista.matchningdata);
+}
+
 async function searchByWords(criteria) {
-  //
   // Should split search string by _space_ or commas
   // const newCriteria = splitSearchString();
-  //
-  const searchURL = `platsannonser/matchning?nyckelord=${criteria}`;
+
+  //concat free word search term!
+  const searchURL = `nyckelord=${criteria}`;
   console.log(searchURL);
+  // fetch data!
   const matches = await fetchData(searchURL);
   return matches;
 }
+async function searchByLan(criteria) {
+  console.log(criteria);
+  const searchURL = `lanid=${criteria}`
+  const matches = await fetchData(searchURL);
+  return matches;
+}
+
+async function searchByYrkesomrade(criteria) {
+  console.log(criteria);
+  const searchURL = `yrkesomradeid=${criteria}`
+  const matches = await fetchData(searchURL);
+  return matches;
+}
+
 async function fetchData(criteria) {
   //sets base URL:
-  const baseURL = `http://api.arbetsformedlingen.se/af/v0/`;
+  const baseURL = `http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?`;
   //creates array of search resutl objects
   const responseObject = await fetch(baseURL + criteria);
   const matches = await responseObject.json();
   return matches;
 }
+
+// This function is set on hold
+// For now we hard code buttons with ids similar to lanID from the API
+//
+// async function getLanID(lanName){
+//   const lanURL= `platsannonser/soklista/lan`;
+//   const lanList = await fetchData(lanURL);
+//   console.log(lanList)
+//       for (let lan of lanList.soklista.sokdata){
+//         console.log(`${lan.id} tillhör ${lan.namn}` )
+//       }
+// }
