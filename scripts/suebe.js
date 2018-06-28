7ed65d97f232aa5f6338f36e151f6c6bd0226d96
Panic3n <jens.nords@gmail.com>
2018-06-28 15:53:01 +0200

    last-minute-fix

--- scripts/suebe.js
+++
@@ -1,9 +0,0 @@
-let numberOfAds = document.querySelector('#quantityOfAdsDiv');
-
-function countingAds (){
-  let i;
-  for (i = 0; i < matchningslista.antal_platsannonser.length; i++)
-  let adCount = matchningslista.antal_platsannonser;
-  numberOfAds.innerHTML(afterbegin, (´10 av totalt´) + adCount)
-
-};
